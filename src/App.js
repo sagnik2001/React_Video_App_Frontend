0a832848b445e78a8081914a7b61561415e2222e
Akash <bagakash11@gmail.com>
2023-04-11 15:27:16 +0530

    debu is bokachoda

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import React from "react";
 
 const App = () => {
-  return (
-  <h1>
-    Hlw from akash
-    HI motherFucker!!
-    </h1>
-    );
+  return <h1>Hlw from akash HI motherFucker!! bokachoda</h1>;
 };
 
 export default App;
