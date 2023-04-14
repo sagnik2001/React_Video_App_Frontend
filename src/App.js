fc811068ff992dcdc4639e6271a512d24ad49ecd
Sagnikpal <74464019+sagnik2001@users.noreply.github.com>
2023-04-14 12:10:32 +0530

    New Setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from "react";
 import Sidebar from "./Sidebar";
+import { BrowserRouter,Routes,Route } from "react-router-dom";
+import RegisterPage from "./Pages/Register";
 
 const App = () => {
   return (
-    <div className="">
-      {/* sidebar */}
-      <Sidebar />
-      {/* chat Section */}
-    </div>
+    // <div className="">
+    //   {/* sidebar */}
+    //   {/* <Sidebar /> */}
+    //   {/* chat Section */}
+    // </div>
+    <BrowserRouter>
+        <Routes>
+          <Route path="/login"  element={<RegisterPage/>}/>
+        </Routes>
+    </BrowserRouter>
   );
 };
 
