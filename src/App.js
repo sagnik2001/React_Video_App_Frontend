59c52e2aea46daf081ef8dc8512ac81e2e8af469
Soumi Biswas <soumiisnoone@gmail.com>
2023-04-15 23:10:53 +0530

    soumi_rooms_changes

--- src/App.js
+++ src/App.js
@@ -4,35 +4,43 @@ import "./App.css"
 import { BrowserRouter,Routes,Route } from "react-router-dom";
 import Register from "./Pages/register/Register";
 import LogInPage from "./Pages/Login/LogInPage";
+import Rooms from "./Pages/rooms/Rooms";
+import contents from "./Pages/rooms/content";
 
 const App = () => {
-  const [currentForm, setCurrentForm] = useState('login');
+  // const [currentForm, setCurrentForm] = useState('login');
 
-  const toggleForm = (formName) => {
-    setCurrentForm(formName)
-  }
-
-
-
+  // const toggleForm = (formName) => {
+  //   setCurrentForm(formName)
+  // }
+  
   return (
-    
     // <div className="">
     //   {/* sidebar */}
     //   {/* <Sidebar /> */}
     //   {/* chat Section */}
     // </div>
-    
-    <BrowserRouter>
-    <div className="App">
-      {
-            currentForm === "login" ? <LogInPage onFormSwitch={toggleForm} /> : <Register onFormSwitch={toggleForm} />
-      }
-          </div>
-        <Routes>
-          <Route exact path="/login"  element={<LogInPage />}></Route>
-          <Route path="/Register"  element={<Register />}/>
-        </Routes>
-    </BrowserRouter>
+    <div>
+    {contents.map(contents => (
+        <Rooms 
+            key={contents.id}
+            name={contents.name}
+            participants={contents.participants}
+        />
+    ))}
+  </div>
+
+    // <BrowserRouter>
+    // <div className="App">
+    //   {
+    //         currentForm === "login" ? <LogInPage onFormSwitch={toggleForm} /> : <Register onFormSwitch={toggleForm} />
+    //   }
+    //       </div>
+    //     <Routes>
+    //       <Route exact path="/login"  element={<LogInPage />}></Route>
+    //       <Route path="/Register"  element={<Register />}/>
+    //     </Routes>
+    // </BrowserRouter>
   );
 };
 
