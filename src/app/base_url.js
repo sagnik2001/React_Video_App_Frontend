5e239126019fe5628f154dcbb739ce63f3ed2ed5
Sagnik Pal <palsagnik2001@gmail.com>
2024-04-05 13:42:57 +0530

    Sagnik/changes

--- src/app/base_url.js
+++ src/app/base_url.js
@@ -1,2 +1,2 @@
-// export const base_url = "https://videoappbackend.onrender.com/"
-export const base_url = "http://localhost:8080/"
+export const base_url = "https://videoappbackend.onrender.com/"
+// export const base_url = "http://localhost:8080/"
